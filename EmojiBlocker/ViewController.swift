807e7a135ec1424a3954192eed2e736602ccc9e6
nemsothea <nemsothea13@gmail.com>
2022-08-30 08:52:05 +0900

    - Emoji blocker

--- EmojiBlocker/ViewController.swift
+++ EmojiBlocker/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /* TODO :
+     - Outlet
+    */
+    
+    @IBOutlet weak var MyTextView   : UITextView!
+    @IBOutlet weak var MyLabelError : UILabel!
+    @IBOutlet weak var MyDoneButton : UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
+    /* TODO :
+     - Action
+    */
+    
+    @IBAction func doneAction(_ sender: Any) {
+        
+        
+        
+    }
+    
 
 }
 
