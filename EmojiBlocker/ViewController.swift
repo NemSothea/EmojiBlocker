5e6c3e99b528c3e190e6ac5c282245676ece3742
nemsothea <nemsothea13@gmail.com>
2022-08-30 16:35:15 +0900

    echo "# EmojiBlocker" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/NemSothea/EmojiBlocker.git git push -u origin main

--- EmojiBlocker/ViewController.swift
+++ EmojiBlocker/ViewController.swift
@@ -21,6 +21,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.MyLabelError.isHidden  = true
+        
+        self.MyDoneButton.layer.cornerRadius = 12
+        self.MyDoneButton.layer.borderWidth = 1
+        self.MyDoneButton.layer.borderColor = UIColor.gray.cgColor
+        
+//        self.MyTextView.keyboardType = .asciiCapable = Displays a keyboard which can enter ASCII characters
+        self.MyTextView.layer.cornerRadius = 5
+        self.MyTextView.delegate    = self
+        
         
     }
     /* TODO :
@@ -29,10 +39,25 @@ class ViewController: UIViewController {
     
     @IBAction func doneAction(_ sender: Any) {
         
-        
+        self.MyTextView.resignFirstResponder()
         
     }
     
 
 }
-
+extension ViewController : UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if containValidCharacters(text) {
+            self.MyLabelError.isHidden = true
+            return true
+        }
+        self.MyLabelError.isHidden = false
+        return false
+    }
+    private func containValidCharacters(_ text : String) -> Bool {
+        let validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,@:?!()$\\/# \n"
+        let inValidSet = CharacterSet(charactersIn: validChars).inverted
+        return text.rangeOfCharacter(from: inValidSet) == nil
+    }
+    
+}
